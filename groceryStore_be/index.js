e666b04daf1caa2b7b6cac438beb53c3155d8441
Ankur Mishra <99202067+ankurmishra29@users.noreply.github.com>
2025-09-08 22:39:45 +0530

    Update index.js
    fixing cors issue

--- groceryStore_be/index.js
+++ groceryStore_be/index.js
@@ -7,14 +7,31 @@ const router = require("./routes");
 const cookiesParser = require("cookie-parser");
 
 const app = express();
+
+const allowedOrigins = [
+  process.env.FRONTEND_URL,
+  'http://localhost:3000',
+  'http://localhost:5173', 
+  'https://grocerystoreank.netlify.app'
+].filter(Boolean);
+
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      } else {
+        console.log('CORS blocked origin:', origin);
+        return callback(new Error('Not allowed by CORS'));
+      }
+    },
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    allowedHeaders: ["Content-Type", "Authorization", "Cookie"],
   })
 );
+
 app.use(express.json());
 app.use(cookiesParser());
 app.use("/api", router);
